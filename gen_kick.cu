e70a542aff72d84b74bc53124d5441179e7a04e3
FukutaTetsuya <fukuta@r.phys.nagoya-u.ac.jp>
2020-01-17 19:22:04 +0900

    gen_kick.cuをh_kick_storageに変数をまとめて作るように書き換えた

--- gen_kick.cu
+++ gen_kick.cu
@@ -9,44 +9,39 @@
 
 __device__ __constant__ int d_Np;
 
-__global__ void show_inside_d_array_kick(int d_N_active, float *d_array_kick) {
-	int i = threadIdx.x + blockIdx.x * blockDim.x;
-	float x, y;
-
-	for(; i < d_N_active; i += NUM_BLOCK + NUM_THREAD) {
-		x = d_array_kick[i * 2];
-		y = d_array_kick[i * 2 + 1];
-		if(i < 10) {
-			printf("%d:%f,%f\n", i, x, y);
-		}
-	}
-}
-
-void gen_array_kick(curandGenerator_t gen_mt, int d_N_active, float *d_array_kick, float *h_array_kick) {
+void gen_array_kick(curandGenerator_t gen_mt, float *h_kick_storage, size_t storage_size) {
 	float *d_array_temp;
 	float *h_array_temp;
-	size_t n = d_N_active * 2;
+	float *h_array_kick;
+	size_t generate_size = 16384;
 	int i, j;
 	int if_break;
+	int starting_point, ending_point;
 	float x, y;
 	double l;
 
-	if(cudaSuccess != cudaMalloc((void **)&d_array_temp, n * sizeof(float))) {
+	if(cudaSuccess != cudaMalloc((void **)&d_array_temp, generate_size * sizeof(float))) {
 		printf("failed to alloc on device\n");
 	}
-	h_array_temp = (float *)calloc(n , sizeof(float));
-	if(h_array_temp == NULL) {
+	h_array_temp = (float *)calloc(generate_size , sizeof(float));
+	h_array_kick = (float *)calloc(generate_size , sizeof(float));
+	if(h_array_temp == NULL || h_array_kick == NULL) {
 		printf("failed to alloc on host\n");
 	}
 
 	i = 0;
 	if_break = 0;
-	while(i < n) {
-		curandGenerateUniform(gen_mt, d_array_temp, n);
-		cudaDeviceSynchronize();
-		cudaMemcpy(h_array_temp, d_array_temp, n * sizeof(float), cudaMemcpyDeviceToHost);
+	starting_point = 0;
+	ending_point= 0;
 
-		for(j = 0; j < n; j += 2) {
+	while(i < storage_size) {
+		curandGenerateUniform(gen_mt, d_array_temp, generate_size);
+		cudaDeviceSynchronize();
+		cudaMemcpy(h_array_temp, d_array_temp, generate_size * sizeof(float), cudaMemcpyDeviceToHost);
+
+		starting_point = ending_point;
+		i = 0;
+		for(j = 0; j < generate_size; j += 2) {
 			x = 1.0 - 2.0 * h_array_temp[j];
 			y = 1.0 - 2.0 * h_array_temp[j + 1];
 			l = x * x + y * y;
@@ -55,32 +50,64 @@ void gen_array_kick(curandGenerator_t gen_mt, int d_N_active, float *d_array_kic
 				h_array_kick[i] = x * l * 0.5;
 				h_array_kick[i + 1] = y * l * 0.5;
 				i += 2;
+				ending_point += 2;
 			}
-			if(i >= n - 1) {
+			if(ending_point >= storage_size - 1) {
 				if_break = 1;
+				printf("broke at ending_point:%d\n", ending_point);
 				break;
 			}
 		}
+		memcpy(&h_kick_storage[starting_point], h_array_kick, i * sizeof(float));
 		if(if_break == 1) {
 			break;
 		}
 	}
 
-	cudaMemcpy(d_array_kick, h_array_kick, n * sizeof(float), cudaMemcpyHostToDevice);
 
 	cudaFree(d_array_temp);
 	free(h_array_temp);
+	free(h_array_kick);
+}
+
+void make_histogram(double max_value, float *array, int dim_array) {
+	int i, j;
+	double delta;
+	double dx, dy, dr;
+	int histogram[100];
+	int place;
+	delta = max_value / 100;
+	for(i = 0; i < 100; i += 1) {
+		histogram[i] = 0;
+	}
+
+	for(i = 0; i < dim_array; i += 2) {
+		dx = array[i];
+		dy = array[i + 1];
+		dr = sqrt(dx * dx + dy * dy);
+		place = (int)(dr / delta);
+		if(place < 99) {
+			histogram[place] += 1;
+		} else {
+			histogram[99] += 1;
+		}
+	}
+	for(i = 0; i < 100; i += 1) {
+		printf("%d:%d\n", i, histogram[i]);
+	}
 }
 
 int main(void) {
-	float *d_array_kick;
-	float *h_array_kick;
-	int d_N_active = 5240;//indeed it's on host
-	int n = d_N_active * 2;
+	double dx, dy, dr;
+	float *h_kick_storage;
+	size_t storage_size = 1600000;
 	curandGenerator_t gen_mt;
 	
-	cudaMalloc((void **)&d_array_kick, n * sizeof(float));
-	h_array_kick = (float *)calloc(n, sizeof(float));
+	h_kick_storage = (float *)calloc(storage_size, sizeof(float));
+	if(h_kick_storage == NULL) {
+		printf("failed to allocate h_kick_storage\n");
+		return 0;
+	}
 
 	//set up generator
 	curandCreateGenerator(&gen_mt, CURAND_RNG_PSEUDO_MTGP32);
@@ -88,19 +115,29 @@ int main(void) {
 	curandSetPseudoRandomGeneratorSeed(gen_mt, (unsigned long)time(NULL));
 	curandSetGeneratorOffset(gen_mt, 0ULL);
 	curandSetGeneratorOrdering(gen_mt, CURAND_ORDERING_PSEUDO_DEFAULT);
-	printf("set-ed gene\n");
+	printf("set-ed generator\n");
 
 	//make d_N_active random kick 
-	gen_array_kick(gen_mt, d_N_active, d_array_kick, h_array_kick);
+	gen_array_kick(gen_mt, h_kick_storage, storage_size);
 	cudaDeviceSynchronize();
 	printf("gen-ed kick array\n");
 
-	show_inside_d_array_kick<<<NUM_BLOCK, NUM_THREAD>>>(d_N_active, d_array_kick);
+	dx = h_kick_storage[0];
+	dy = h_kick_storage[1];
+	dr = sqrt(dx * dx + dy * dy);
+	printf("%f %f %f\n", dx ,dy ,dr);
+	dx = h_kick_storage[storage_size - 2];
+	dy = h_kick_storage[storage_size - 1];
+	dr = sqrt(dx * dx + dy * dy);
+	printf("%f %f %f\n", dx ,dy ,dr);
+
+
+	make_histogram(0.5, h_kick_storage, storage_size);
+
 
 	//finalize
 	curandDestroyGenerator(gen_mt);
-	cudaFree(d_array_kick);
-	free(h_array_kick);
+	free(h_kick_storage);
 	printf("finalized\n");
 	return 0;
 }
